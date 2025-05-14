e23b9ce330750e2ad143240c84dfff1c5d50176c
cerdocebolla <lordofsuperium17@gmail.com>
2025-05-14 14:02:57 +0000

    Added new protection files

---
+++ piallo-vf3-mainFolders/cloudflareAdditionalProtection.rs
@@ -0,0 +1,23 @@
+local function.askForProtection.LocalClient - setApp 
+as CloudFlare = allowAccessToTheScript.force.setTrue = true (alwaysSetTrue)
+                when askedForParent = protectedWiFi.true.forceToTrue = setTrue.force = true 
+                then let function = CloudFlare.allowAccessToTheScript = alwaysSetTrue.force true 
+                for CloudFlare
+            when accessGiven.connectClient.localClient.LocalDevice = seperateParent.chainedWiFi = true (alwaysSetTrue)
+            set getParent.force = true 
+            set parent 
+            as CloudFlare = forceToTrue = true (alwaysSetTrue)
+    when accessGiven.controlClient.LocalWiFi = setMainDevice as 
+    goInBracket = (seperateParent.SetParent = Modem = forceToTrue) = true (alwaysSetTrue)
+    when Modem.connectedAsDevice then 
+    SetMainDevice.chainedDevice.ChainedClient.LocalServers = accessGiven for 
+    CloudFlare = true 
+    then let endInstant
+}
+    else runSoftware
+    for CloudFlare 
+        try.RunningAlways.SoftwareChain.blockChain = true 
+        when 
+        as setTrue then let 
+    endInstant
+}
---
+++ piallo-vf3-mainFolders/protectionHandler.bat
@@ -0,0 +1,8 @@
+if function.Local workAsHardware = accessCloudFlareNetwork 
+        then accessWiFi.connect.ChainedConnection = setTrue 
+        as set 
+        true 
+        then endInstant.end 
+        end)
+    end)
+}
