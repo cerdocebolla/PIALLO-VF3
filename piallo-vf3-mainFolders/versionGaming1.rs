ad051d0eac5321e3e46b399906a41f919efc90e9
cerdocebolla <lordofsuperium17@gmail.com>
2025-05-14 20:22:04 +0000

    Added files for new release

---
+++ piallo-vf3-mainFolders/versionGaming1.rs
@@ -0,0 +1,18 @@
+let function = addAdditionalFiles = setTrue = true 
+when askedForParent = SetAnswersForParent = true 
+                then let Run.MainDevice.localClients.LocalServers.RunSoftware = (pialloForGaming.rs) 
+                when askedFor (goUp.AskedForChainedClient) goInBracket = then let 
+                as CreateNewParent.Parent.force.forceToTrue.force = true (alwaysSetTrue)
+        for newFunction.CreateNewParent = CreateChain.NewChain.whenAsked.DirectConnect.Answer then = true 
+        CreateChain for 
+    as MainUser.MainWiFi.connectionMade then RunSoftware.PIALLO.WhenRunning.StartAddingChild.GamingFunctions.ForCause = runSoftware.pialloForGaming
+    whemn goUp = true then 
+    let function 
+    as let 
+}
+else then 
+let print ("Couldn't run the software PIALLO for Gaming, please try again or restart your main device.")
+then function.DirectConnect.ChainedClient.chainedDevice.ChainedWiFi.DirectConnect = Chain.force.forceToTrue.force = true (alwaysSetTrue)
+                                                        then let function 
+                                                        let endInstant
+}
