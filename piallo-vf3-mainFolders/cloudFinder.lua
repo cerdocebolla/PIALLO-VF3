c5bad8a3a0e00625b1d1b2b25354c2b571ce074e
cerdocebolla <lordofsuperium17@gmail.com>
2025-05-16 20:22:54 +0000

    Added new files for the new release

---
+++ piallo-vf3-mainFolders/cloudFinder.lua
@@ -0,0 +1,39 @@
+when askedForFunction = setTrue = true 
+then let function = setAslocal.chain 
+    when goDown then createChain = forceToTrue = true
+        when chain = setAs sealed 
+        and created 
+        if 
+
+    function = runningTheSoftware = true 
+        then createChain in 
+            as down 
+            localServers.cloudServers.AlwaysAccess.localClient.client.Servers.server.mainDevice.mainUsers.user = true 
+            then let 
+                function endInstant.end)
+            end)
+        end)
+    end)
+}
+else local client.function = false 
+    then function = let print 
+        print ("Couldn't find the cloud servers around, please stand by for an official fixage or try again.")
+        then as fucntion 
+            let function endInstant.end
+        end)
+    end)
+}
+
+when function = letPrintOut = makeTrueWhenSure = accessMade
+    connects 
+    as accessMade = makeTrueWhenSure = true 
+    
+    as function = whenSure 
+        function 
+            let print ("The PIALLO Software has successfully accessed into the cloud servers. Running PIALLO...")
+            then function 
+                function endInstant.end
+            end)
+        end)
+    end)
+}
--- piallo-vf3-mainFolders/pialloForGaming.rs
+++ piallo-vf3-mainFolders/pialloForGaming.rs
@@ -14,33 +14,33 @@ setTrue = true
     when functions = runProper.runExeGaming = true 
     (setTrue = true) goOutBracket 
     then askForGame = supportGame
-    then connect.mainDevice.mainUser.computer = DirectConnect.Steam = setTrue.force alwaysSetTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.EpicGames = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.UPlay = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.BattleNet = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.RockstarGames = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.GOG = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.Origin = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.WindowsStore = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.XboxGamePass = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.AmazonLuna = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.Stadia = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.GeforceNow = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.PlayStationNow = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.NVIDIAShield = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.OculusLink = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.VRChat = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.SteamVR = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.VivePort = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.OculusStore = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.HPReverbG2 = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.WMR = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.Pimax = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.Index = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.ViveCosmos = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestLink = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestAirLink = setTrue.forceAlwaysTrue
-    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestVirtualDesktop = setTrue.forceAlwaysTrue
+    then connect.mainDevice.mainUser.computer = DirectConnect.Steam = setTrue.force alwaysSetTrue = true 
+    then connect.mainDevice.mainUser.computer = DirectConnect.EpicGames = setTrue.forceAlwaysTrue = true 
+    then connect.mainDevice.mainUser.computer = DirectConnect.UPlay = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.BattleNet = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.RockstarGames = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.GOG = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Origin = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.WindowsStore = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.XboxGamePass = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.AmazonLuna = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Stadia = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.GeforceNow = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.PlayStationNow = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.NVIDIAShield = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusLink = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.VRChat = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.SteamVR = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.VivePort = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusStore = setTrue.forceAlwaysTrue  = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.HPReverbG2 = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.WMR = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Pimax = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Index = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.ViveCosmos = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestLink = setTrue.forceAlwaysTrue = true 
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestAirLink = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestVirtualDesktop = setTrue.forceAlwaysTrue = true
 
     when goUp (checkUp) = setTrue.force alwaysSetTrue = true 
     then seeFunction = run 
